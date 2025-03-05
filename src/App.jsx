e43e840c879778ec2c81d194d785da5b4e04da30
Dhruvi-2699 <ramidhruvi26@gmail.com>
2025-03-05 15:28:57 +0530

    keep cartITem even on refresing using localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,29 @@ import React, { useEffect } from "react";
 import CartItem from "./components/CartItem";
 import { useCart } from "./context/cartContext";
 import ShoppingCart from "./components/ShoppingCart";
+import {
+  getItemFromStorage,
+  getParsedItemFromStorage,
+} from "./assets/utility/localStorageFns";
 
 const App = () => {
-  const { allItems, setItems } = useCart();
+  const { allItems, setItems, handleCartItemFromStorage } = useCart();
   useEffect(() => {
     setItems();
+    if (
+      getParsedItemFromStorage("cartItems")?.length !== 0 &&
+      getItemFromStorage("cartItems") !== null
+    ) {
+      handleCartItemFromStorage();
+    }
   }, []);
-  useEffect(() => {
-    console.log(allItems);
-  }, [allItems]);
   return (
-    <div className="grid text-center">
-      <h1 className="text-4xl italic text-gray-500 my-8">
+    <div className="grid place-items-center py-20">
+      <h1 className="lg:text-4xl md:text-3xl text-2xl italic text-gray-500 text-center p-5">
         Trend Alert : Treading Outfit of the Season
       </h1>
       <ShoppingCart />
-      <div className="grid md:grid-cols-3 place-items-start gap-10 m-4">
+      <div className="grid xl:grid-cols-3 lg:grid-cols-2 place-items-start gap-10 xl:px-6 px-10">
         {allItems?.map((item) => {
           return <CartItem key={item.id} item={item} />;
         })}
--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -1,5 +1,10 @@
 import { createContext, useContext, useState } from "react";
 import { allProducts } from "../assets/utility";
+import {
+  getItemFromStorage,
+  getParsedItemFromStorage,
+  setItemInStorage,
+} from "../assets/utility/localStorageFns";
 
 const CartContext = createContext();
 export const CartProvider = ({ children }) => {
@@ -39,9 +44,36 @@ export const CartProvider = ({ children }) => {
       });
     });
   };
+  const handleLocalStorage = () => {
+    if (allItems.length !== 0) {
+      const inCartItems = allItems.filter((item) => item.inCart);
+      setItemInStorage("cartItems", inCartItems);
+    }
+  };
+  const handleCartItemFromStorage = () => {
+    if (getItemFromStorage("cartItems") !== null) {
+      const storageItems = getParsedItemFromStorage("cartItems");
+      setAllItems((prevItems) => {
+        return prevItems.map((item) => {
+          const matchedItem = storageItems.find(
+            (storageItem) => storageItem.id == item.id
+          );
+          return matchedItem ? matchedItem : item;
+        });
+      });
+    }
+  };
   return (
     <CartContext.Provider
-      value={{ allItems, setItems, addToCart, removeFromCart, updateQuantiy }}
+      value={{
+        allItems,
+        setItems,
+        addToCart,
+        removeFromCart,
+        updateQuantiy,
+        handleLocalStorage,
+        handleCartItemFromStorage,
+      }}
     >
       {children}
     </CartContext.Provider>
