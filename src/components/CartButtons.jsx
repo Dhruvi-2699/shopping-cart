188601a22c33f6ecfc328ff882efecee69ffb8f4
Dhruvi-2699 <ramidhruvi26@gmail.com>
2025-03-05 12:56:25 +0530

    update design in cartItem for cart & normal use. calculate totalprice of cart products

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useCart } from "../context/cartContext";
 
-const CartButtons = ({ item }) => {
+const CartButtons = ({ item, fromCart }) => {
   const { addToCart, removeFromCart, updateQuantiy } = useCart();
 
   return (
-    <div className="w-max absolute right-5 top-5">
+    <div className={`w-max absolute right-5 top-5 ${fromCart && "scale-70"}`}>
       <div className="space-x-3">
         {!item.inCart ? (
           <button
--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -6,15 +6,19 @@ import CartItem from "./CartItem";
 const ShoppingCart = () => {
   const [isOpens, setIsOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
   const { allItems } = useCart();
   console.log("check-", cartItems);
 
   useEffect(() => {
     console.log("check-ok", allItems);
-
     const inCartItems = allItems.filter((item) => item.inCart == true);
-
     setCartItems(inCartItems?.length > 1 ? inCartItems : []);
+    const price = inCartItems.reduce((accumulator, item) => {
+      return (accumulator += item.price * item.quantity);
+    }, 0);
+    setTotalPrice(price);
   }, [allItems]);
 
   return (
@@ -44,11 +48,11 @@ const ShoppingCart = () => {
         </div>
         <div className="h-screen flex flex-col gap-y-3 overflow-y-scroll px-5 pb-24 pt-20">
           {cartItems?.map((item) => {
-            return <CartItem key={item.id} item={item} />;
+            return <CartItem key={item.id} item={item} fromCart={true} />;
           })}
         </div>
         <div className="w-full h-20 bg-white absolute bottom-0 left-0 z-10 grid place-items-center border border-white rounded-lg">
-          <h1 className="text-xl text-gray-600">Total : $155</h1>
+          <h1 className="text-xl text-gray-600">Total : {totalPrice}</h1>
           <button className="rounded-md bg-blue-300 px-2 text-white hover:bg-blue-100 transition-colors">
             Buy now
           </button>
